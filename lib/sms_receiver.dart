01624e512b70981471727bc3732d2c9a0646cdff
kirya355 <41366638+kirya355@users.noreply.github.com>
2021-05-18 16:45:14 +0300

    null safety

--- lib/sms_receiver.dart
+++ lib/sms_receiver.dart
@@ -1,14 +1,12 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class SmsReceiver {
-  static const MethodChannel _channel =
-      const MethodChannel('com.oval.smsreceiver');
+  static const MethodChannel _channel = const MethodChannel('com.oval.smsreceiver');
 
   Function(String) onSmsReceived;
-  Function onTimeout;
-  Function onFailureListener;
+  Function? onTimeout;
+  Function? onFailureListener;
 
   SmsReceiver(this.onSmsReceived, {this.onTimeout, this.onFailureListener}) {
     _channel.setMethodCallHandler(_handleMethod);
@@ -17,28 +15,21 @@ class SmsReceiver {
   Future<dynamic> _handleMethod(MethodCall call) async {
     switch (call.method) {
       case "onSmsReceived":
-        if (onSmsReceived != null) {
-          onSmsReceived(call.arguments);
-        }
+        if (call.arguments is String) onSmsReceived(call.arguments);
         break;
 
       case "onTimeout":
         if (onTimeout != null) {
-          onTimeout();
+          onTimeout!();
         }
         break;
 
       case "onFailureListener":
         if (onFailureListener != null) {
-          onFailureListener();
+          onFailureListener!();
         }
         break;
     }
-  }
-
-  Future<String> platformVersion() async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
   }
 
   Future<bool> startListening() async {
